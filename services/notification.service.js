7d8372fe551d48d656035f6904c7f8e6eecdebd9
徐红飞 <985992884@qq.com>
2018-02-02 15:29:15 +0800

    1

--- services/notification.service.js
+++ services/notification.service.js
@@ -1,44 +1,109 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var material_1 = require("@angular/material");
-var NotificationService = /** @class */ (function () {
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { Injectable } from "@angular/core";
+import { MatSnackBar, MatSnackBarConfig } from "@angular/material";
+import { TdDialogService } from "@covalent/core";
+/**
+ * 封装各种消息推送方法
+ */
+var NotificationService = (function () {
     function NotificationService(snackBar, dialogService) {
         this.snackBar = snackBar;
         this.dialogService = dialogService;
-        this._snackBarConfig = new material_1.MdSnackBarConfig();
+        this._snackBarConfig = new MatSnackBarConfig();
         this._snackBarConfig.duration = 4000;
+        Notification.requestPermission().then(function (_) {
+            if (_ === 'denied') {
+                // throw new Error('目前无法接收任何通知');
+            }
+        });
     }
-    NotificationService.prototype.open = function (message, cssClass) {
-        this._snackBarConfig.extraClasses = cssClass;
-        this.snackBar.open(message, '关闭', this._snackBarConfig);
-    };
-    NotificationService.prototype.success = function (message) {
+    /**
+     * @param {?} message
+     * @return {?}
+     */
+    NotificationService.prototype.success = /**
+     * @param {?} message
+     * @return {?}
+     */
+    function (message) {
         this.open(message, ['bgc-green-A700']);
     };
-    NotificationService.prototype.error = function (message) {
+    /**
+     * @param {?} message
+     * @return {?}
+     */
+    NotificationService.prototype.error = /**
+     * @param {?} message
+     * @return {?}
+     */
+    function (message) {
         this.open(message, ['bgc-red-A700']);
     };
-    NotificationService.prototype.warn = function (message) {
-        this.open(message, ['bgc-yellow-A700']);
+    /**
+     * @param {?} message
+     * @return {?}
+     */
+    NotificationService.prototype.warn = /**
+     * @param {?} message
+     * @return {?}
+     */
+    function (message) {
+        this.open(message, ['bgc-yellow-A700', 'tc-grey-900']);
     };
-    NotificationService.prototype.info = function (message) {
+    /**
+     * @param {?} message
+     * @return {?}
+     */
+    NotificationService.prototype.info = /**
+     * @param {?} message
+     * @return {?}
+     */
+    function (message) {
         this.open(message, ['bgc-blue-A700']);
     };
-    NotificationService.prototype.confirmDelete = function (title, acceptButton, cancelButton) {
+    /**
+     * 是否确认删除弹窗方法
+     * @param {?=} title          显示标题
+     * @param {?=} acceptButton   确认按钮
+     * @param {?=} cancelButton   取消按钮
+     * @return {?}
+     */
+    NotificationService.prototype.confirmDelete = /**
+     * 是否确认删除弹窗方法
+     * @param {?=} title          显示标题
+     * @param {?=} acceptButton   确认按钮
+     * @param {?=} cancelButton   取消按钮
+     * @return {?}
+     */
+    function (title, acceptButton, cancelButton) {
         if (title === void 0) { title = '请确认'; }
         if (acceptButton === void 0) { acceptButton = '确定'; }
         if (cancelButton === void 0) { cancelButton = '取消'; }
         return this.confirm('确定要删除这条记录?', title, acceptButton, cancelButton).filter(function (_) { return _; });
     };
-    NotificationService.prototype.confirm = function (message, title, acceptButton, cancelButton, disableClose) {
-        if (title === void 0) { title = '请确认'; }
+    /**
+     * 自定义提醒弹窗内容
+     * @param {?} message
+     * @param {?=} title
+     * @param {?=} acceptButton
+     * @param {?=} cancelButton
+     * @param {?=} disableClose
+     * @return {?}
+     */
+    NotificationService.prototype.confirm = /**
+     * 自定义提醒弹窗内容
+     * @param {?} message
+     * @param {?=} title
+     * @param {?=} acceptButton
+     * @param {?=} cancelButton
+     * @param {?=} disableClose
+     * @return {?}
+     */
+    function (message, title, acceptButton, cancelButton, disableClose) {
+        if (title === void 0) { title = '温馨提示'; }
         if (acceptButton === void 0) { acceptButton = '确定'; }
         if (cancelButton === void 0) { cancelButton = '取消'; }
         if (disableClose === void 0) { disableClose = false; }
@@ -48,21 +113,27 @@ var NotificationService = /** @class */ (function () {
             title: title,
             acceptButton: acceptButton,
             cancelButton: cancelButton,
-        }).afterClosed().filter(function (_) { return _; });
+        }).afterClosed();
     };
-    NotificationService.prototype.confirmUnbind = function (title, acceptButton, cancelButton) {
-        if (title === void 0) { title = '请确认'; }
-        if (acceptButton === void 0) { acceptButton = '确定'; }
-        if (cancelButton === void 0) { cancelButton = '取消'; }
-        return this.confirm('确定要解除绑定?', title, acceptButton, cancelButton).filter(function (_) { return _; });
-    };
-    NotificationService.prototype.confirmSub = function (title, acceptButton, cancelButton) {
-        if (title === void 0) { title = '请确认'; }
-        if (acceptButton === void 0) { acceptButton = '确定'; }
-        if (cancelButton === void 0) { cancelButton = '取消'; }
-        return this.confirm('确定提交?', title, acceptButton, cancelButton).filter(function (_) { return _; });
-    };
-    NotificationService.prototype.prompt = function (message, value, title, acceptButton, cancelButton) {
+    /**
+     * 用于提示还未完成的操作内容
+     * @param {?} message   提示的未完成的内容
+     * @param {?=} value
+     * @param {?=} title
+     * @param {?=} acceptButton
+     * @param {?=} cancelButton
+     * @return {?}
+     */
+    NotificationService.prototype.prompt = /**
+     * 用于提示还未完成的操作内容
+     * @param {?} message   提示的未完成的内容
+     * @param {?=} value
+     * @param {?=} title
+     * @param {?=} acceptButton
+     * @param {?=} cancelButton
+     * @return {?}
+     */
+    function (message, value, title, acceptButton, cancelButton) {
         if (value === void 0) { value = ''; }
         if (title === void 0) { title = '请补充'; }
         if (acceptButton === void 0) { acceptButton = '确定'; }
@@ -76,9 +147,71 @@ var NotificationService = /** @class */ (function () {
             acceptButton: acceptButton,
         }).afterClosed();
     };
-    NotificationService = __decorate([
-        core_1.Injectable()
-    ], NotificationService);
+    /**
+     *  弹出通知
+     * @param {?} title  显示标题
+     * @param {?=} body   内容
+     * @param {?=} icon   图标
+     * @param {?=} click  点击事件的回调
+     * @return {?}
+     */
+    NotificationService.prototype.notify = /**
+     *  弹出通知
+     * @param {?} title  显示标题
+     * @param {?=} body   内容
+     * @param {?=} icon   图标
+     * @param {?=} click  点击事件的回调
+     * @return {?}
+     */
+    function (title, body, icon, click) {
+        var /** @type {?} */ payload = new Notification(title, {
+            body: body,
+            icon: icon || 'assets/img/favicon.png'
+        });
+        if (click) {
+            payload.onclick = function (e) {
+                click(e);
+                payload.close();
+            };
+        }
+    };
+    /**
+     * @param {?} message
+     * @param {?} cssClass
+     * @return {?}
+     */
+    NotificationService.prototype.open = /**
+     * @param {?} message
+     * @param {?} cssClass
+     * @return {?}
+     */
+    function (message, cssClass) {
+        this._snackBarConfig.extraClasses = cssClass;
+        this.snackBar.open(message, '关闭', this._snackBarConfig);
+    };
+    NotificationService.decorators = [
+        { type: Injectable },
+    ];
+    /** @nocollapse */
+    NotificationService.ctorParameters = function () { return [
+        { type: MatSnackBar, },
+        { type: TdDialogService, },
+    ]; };
     return NotificationService;
 }());
-exports.NotificationService = NotificationService;
+export { NotificationService };
+function NotificationService_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    NotificationService.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    NotificationService.ctorParameters;
+    /** @type {?} */
+    NotificationService.prototype._snackBarConfig;
+    /** @type {?} */
+    NotificationService.prototype.snackBar;
+    /** @type {?} */
+    NotificationService.prototype.dialogService;
+}
